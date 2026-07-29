cfa19b8a461e30a7ab3a92bd30ab7bcdab55f4a8
Yassin lahhit <yassin.lahhit@outlook.com>
2026-07-29 18:00:08 +0200

    cleanup: remove scratch files and legacy scripts
    Deleted:
    - scratch_schema.php: debug script for table inspection
    - scratch_schema_eventos.php: debug script for eventos table inspection
    - noDeploy/update_db.php: legacy database migration script (superseded by single database.sql)
    - noDeploy/lint.sh: dead script (eslint/stylelint not configured)
    
    All files had zero references and were development-only artifacts.

--- scratch_schema.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once 'modelos/conectar.php';
-$c=obtenerConexion();
-$r=$c->query('SHOW CREATE TABLE configuracion_centro');
-echo $r->fetch_row()[1];
--- scratch_schema_eventos.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'modelos/conectar.php';
-$c=obtenerConexion();
-$r=$c->query('SHOW CREATE TABLE eventos');
-if ($r) {
-    echo $r->fetch_row()[1];
-} else {
-    echo $c->error;
-}
